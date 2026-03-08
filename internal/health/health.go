2f2f4f3bbbe208cb9cd8e2bf73eb3693e25ae72e
72 <72sevenzy2@gmail.com>
2026-03-08 15:13:52 +0500

    finished health checker

--- internal/health/health.go
+++ internal/health/health.go
@@ -1,1 +1,17 @@
 package health
+
+import (
+	"net/http"
+	"fmt"
+)
+
+func HealthHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// ignore requests which arent GET
+		if r.Method != http.MethodGet {
+			http.Error(w, "invalid method", http.StatusMethodNotAllowed)
+		}
+
+		fmt.Fprintln(w, "fully operational API")
+	}
+}
