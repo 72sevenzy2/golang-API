67fdbfb2e5f51520fb9380344124726c90a202ca
72 <72sevenzy2@gmail.com>
2026-03-08 16:11:36 +0500

    finished main entry point

---
+++ cmd/server/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/72sevenzy2/golang-API/internal/handler"
+	"github.com/72sevenzy2/golang-API/internal/health"
+	"github.com/72sevenzy2/golang-API/internal/service"
+)
+
+func main() {
+	service := &service.GreetCounter{}
+
+	http.HandleFunc("/greet", handler.GreetHandler(service))
+	http.HandleFunc("/health", health.HealthHandler())
+
+	fmt.Println("API running on port 8080")
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		panic(err)
+	}
+}
