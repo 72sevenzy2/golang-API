189c7f8d1a9ea2a5edb24b56e7710c40dadbdb1c
72 <72sevenzy2@gmail.com>
2026-03-08 15:30:03 +0500

    finished greet handler

--- internal/handler/greet.go
+++ internal/handler/greet.go
@@ -1,1 +1,27 @@
 package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/72sevenzy2/golang-API/internal/service"
+)
+
+func greetHandler(g service.Greeter) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// check if method is GET or not
+		if r.Method != http.MethodGet {
+			http.Error(w, "invalid method", http.StatusMethodNotAllowed)
+			return
+		}
+		name := r.URL.Query().Get("name")
+		resp, err := g.Greet(name)
+
+		if err != nil {
+			http.Error(w, "bad request", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Fprintln(w, resp)
+	}
+}
