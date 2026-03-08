94a5d129418e16e1d1e58399554cf69fb44039f3
72 <72sevenzy2@gmail.com>
2026-03-08 15:09:22 +0500

    finished

--- internal/service/greeter.go
+++ internal/service/greeter.go
@@ -1,1 +1,29 @@
 package service
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Greeter interface {
+	Greet(name string) (string, error)
+}
+
+type GreetCounter struct {
+	count int
+	mu    sync.Mutex
+}
+
+func (s *GreetCounter) Greet(name string) (string, error) {
+	var count int
+	if name == "" {
+		return "", fmt.Errorf("name cannot be empty")
+	}
+
+	s.mu.Lock()
+	s.count = s.count + 1
+	count = s.count
+	s.mu.Unlock()
+
+	return fmt.Sprintf("welcome back %s, greet number %d", name, count), nil
+}
